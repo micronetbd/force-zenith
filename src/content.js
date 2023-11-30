02a671127d64e4dcbaf9c053d50c0bf3dc68d9d8
Michael Dusaniwskyj <mdu@micronetbd.org>
2023-11-30 12:12:55 -0500

    updated content.js and background.js

--- src/content.js
+++ src/content.js
@@ -1,31 +1,62 @@
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.greeting == "hello") {
-            var con=document.getElementById("forceextension").style.display ;
-            if(con==="none"){
-                document.getElementById("forceextension").style.display   = "block"
-            }else{
-                document.getElementById("forceextension").style.display   = "none"
-            }
-        }
+// chrome.runtime.onMessage.addListener(
+//     function (request, sender, sendResponse) {
+//         console.log(sender.tab ?
+//             "from a content script:" + sender.tab.url :
+//             "from the extension");
+//         if (request.greeting == "hello") {
+//             var con=document.getElementById("forceextension").style.display ;
+//             if(con==="none"){
+//                 document.getElementById("forceextension").style.display   = "block"
+//             }else{
+//                 document.getElementById("forceextension").style.display   = "none"
+//             }
+//         }
 
-        sendResponse({ farewell: "goodbye" });
+//         sendResponse({ farewell: "goodbye" });
+//     }
+// );
+// var substack = document.createElement("iframe");
+// substack.src = chrome.runtime.getURL("src/index.html");
+// substack.style.width = "620px";
+// substack.id = "forceextension";
+// substack.style.height = "100vh";
+// substack.style['z-index'] = "9000";
+// substack.style.position = "absolute";
+// substack.style.border = "none";
+// substack.style.display = "none";
+// substack.style.right = 0;
+// document.body.before(substack);
+
+function toggleDisplay() {
+    var extensionDisplay = document.getElementById("forceextension").style.display;
+    document.getElementById("forceextension").style.display = extensionDisplay === "none" ? "block" : "none";
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(sender.tab ? `Greeting received in content.js from: ${sender.tab.url}` : "Greeting received in content.js from the extension");
+    if (request.greeting === "hello") {
+        toggleDisplay();
+        sendResponse({ message: "Toggle display action performed in content.js" });
     }
-);
-var substack = document.createElement("iframe");
-substack.src = chrome.runtime.getURL("src/index.html");
-substack.style.width = "620px";
-substack.id = "forceextension";
-substack.style.height = "100vh";
-substack.style['z-index'] = "9000";
-substack.style.position = "absolute";
-substack.style.border = "none";
-substack.style.display = "none";
-substack.style.right = 0;
-document.body.before(substack);
+});
+
+function createIframe() {
+    var substack = document.createElement("iframe");
+    substack.src = chrome.runtime.getURL("src/index.html");
+    substack.id = "forceextension";
+    Object.assign(substack.style, {
+        width: "620px",
+        height: "100vh",
+        zIndex: "9000",
+        position: "absolute",
+        border: "none",
+        display: "none",
+        right: "0"
+    });
+    document.body.before(substack);
+}
+
+createIframe(); // Call this function on script load
 
 // Notify background.js that content.js has loaded
-chrome.runtime.sendMessage({ contentScriptReady: true });
+chrome.runtime.sendMessage({ message: "content.js is loaded and ready" });
